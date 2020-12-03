fb303badc64ec2f5e7696452a1fadba87bfd0b33
roy-santos <roysantos112@gmail.com>
2020-12-03 09:20:34 -0800

    Added easy problem 1431

---
+++ src/easy/KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,31 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class KidsWithTheGreatestNumberOfCandies {
+
+    static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int mostCandies = Arrays.stream(candies).max().getAsInt();
+        List<Boolean> canBeTheMost = new ArrayList<Boolean>();
+
+        for (int i=0; i<candies.length; i++) {
+            if(candies[i] + extraCandies >= mostCandies) {
+                canBeTheMost.add(true);
+            }  else {
+                canBeTheMost.add(false);
+            }
+        }
+        return canBeTheMost;
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = new int[]{2, 3, 5, 1, 3};
+        int extraCandies = 3;
+
+        System.out.println(kidsWithCandies(array, extraCandies));
+    }
+
+}
