95d75965a04cbff02350df962d558dbf56a2dbb7
roy-santos <roysantos112@gmail.com>
2020-12-03 21:16:21 -0800

    Add solution for 1108

---
+++ src/easy/DefangingIPAddress.java
@@ -0,0 +1,40 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DefangingIPAddress {
+
+    static String defangIPaddr(String address) {
+        char[] splitAddress = address.toCharArray();
+
+        ArrayList<Character> defangedAddress = new ArrayList<>();
+
+        for (int i=0; i<splitAddress.length; i++) {
+            if (splitAddress[i] == '.') {
+                defangedAddress.add('[');
+                defangedAddress.add('.');
+                defangedAddress.add(']');
+            } else {
+                defangedAddress.add(splitAddress[i]);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<defangedAddress.size(); i++) {
+            sb.append(defangedAddress.get(i));
+        }
+
+        String result = sb.toString();
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        String stringInput = "1.1.1.1";
+
+        System.out.println(defangIPaddr(stringInput));
+    }
+}
