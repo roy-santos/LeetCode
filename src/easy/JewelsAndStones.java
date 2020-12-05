79942abcf7b09f06182522dd0b0f1c012196a0e3
roy-santos <roysantos112@gmail.com>
2020-12-04 18:10:04 -0800

    Add solutions to 771

---
+++ src/easy/JewelsAndStones.java
@@ -0,0 +1,34 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class JewelsAndStones {
+    static int numJewelsInStones(String J, String S) {
+        int jewelCount = 0;
+
+        Set<Character> validJewels = new HashSet<>();
+
+        for (char c : J.toCharArray()) {
+            validJewels.add(c);
+        }
+
+        for (char c : S.toCharArray()) {
+            if (validJewels.contains(c)) {
+                jewelCount+= 1;
+            }
+        }
+
+        return jewelCount;
+    }
+
+    public static void main(String[] args) {
+
+        String jewels = "aA";
+        String stones = "aAAbbbb";
+
+        System.out.println(numJewelsInStones(jewels, stones));
+    }
+}
