3978aad12fc464e1bd61bebc3f866c6c5f4cbd05
roy-santos <roysantos112@gmail.com>
2020-12-05 16:55:59 -0800

    Add solution to 1342

---
+++ src/easy/NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,24 @@
+package easy;
+
+public class NumberOfStepsToReduceANumberToZero {
+
+    static int numberOfSteps (int num) {
+
+        int numMoves = 0;
+
+        while (num > 0) {
+            num = num % 2 == 0 ? num/2 : --num;
+            ++numMoves;
+        }
+
+        return numMoves;
+
+    }
+
+    public static void main (String[] args) {
+
+        int num = 14;
+
+        System.out.println(numberOfSteps(num));
+    }
+}
