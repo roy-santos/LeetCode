3efb3d2803c7d88303405826c1d1003801bbde13
roy-santos <roysantos112@gmail.com>
2020-12-07 20:21:05 -0800

    Add solution to 1528

---
+++ src/easy/ShuffledString.java
@@ -0,0 +1,20 @@
+package easy;
+
+public class ShuffledString {
+    static String restoreString(String s, int[] indices) {
+        char[] shuffled = new char[indices.length];
+
+        for (int i=0; i<indices.length; i++) {
+            shuffled[indices[i]] = s.charAt(i);
+        }
+
+        return String.valueOf(shuffled);
+    }
+
+    public static void main(String[] args) {
+        String s = "codeleet";
+        int[] indices = new int[]{4,5,6,7,0,2,1,3};
+
+        System.out.println(restoreString(s, indices));
+    }
+}
