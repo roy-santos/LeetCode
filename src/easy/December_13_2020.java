8eee5fa9df73a1470ca774ec8b07d61587628ac6
roy-santos <roysantos112@gmail.com>
2020-12-14 11:14:09 -0800

    Solution for 20

--- src/easy/December_13_2020.java
+++ src/easy/December_13_2020.java
@@ -1,8 +1,11 @@
 package easy;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class December_13_2020 {
+
+    // Leetcode 14
     static String longestCommonPrefix(String[] strs) {
         if (strs.length == 0 || strs == null) {
             return "";
@@ -20,9 +23,33 @@ public class December_13_2020 {
         return first;
     }
 
+    // Leetcode 20
+    static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
     public static void main(String[] args) {
         String[] strs = new String[]{"flower", "flow", "flight"};
 
         System.out.println(longestCommonPrefix(strs));
+
+        String s = "(){}[]";
+        String t = "(())(}{}[]";
+
+        System.out.println(isValid(s) == true ? "pass" : "fail");
+        System.out.println(isValid(t) == false ? "pass" : "fail");
     }
 }
